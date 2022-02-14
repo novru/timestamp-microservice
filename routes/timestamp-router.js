59e5e83798f3bd003b0f12678df34c6431b33928
Bruno <bruno.lucas.rodriguez@gmail.com>
2022-02-13 23:42:39 -0300

    query string

--- routes/timestamp-router.js
+++ routes/timestamp-router.js
@@ -3,6 +3,6 @@ const router = express.Router();
 
 const { timestamp } = require("../controllers/timestamp");
 
-router.route("/:date").get(timestamp);
+router.route("/:date?").get(timestamp);
 
 module.exports = router;
